cc9ff2067b0c94c5a5c6189cabd13e0852fdd592
magikboy <massimo.bosco26@gmail.com>
2023-02-02 15:21:33 -0300

    xd

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -6,5 +6,13 @@ en el cuadro de texto "RESULTADO"*/
 //Bosco Mascaro Massimo Ariel Div X
 function mostrarAumento()
 {
-	alert("ok");
+	let num1
+
+    num1 =parseFloat(document.getElementById("txtIdImporte").value);
+
+	resultado= (num1 * 25 / 100 );
+
+	resultado2= (num1 - resultado);
+
+	document.getElementById("txtIdResultado").value=resultado2;
 }
