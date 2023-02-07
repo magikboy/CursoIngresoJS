d63a3bb6b3ec70cd99c2796860757e4844603635
magikboy <massimo.bosco26@gmail.com>
2023-02-07 20:33:41 -0300

    aa

--- 3-InstruccionSwitch/jsInstruccionSwitch-01.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-01.js
@@ -2,7 +2,31 @@ function mostrar()
 {
 	//tomo el mes
 	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+
+	mesDelAño=document.getElementById("txtIdMes").value;
+
+	
+	if(mesDelAño ="Enero" && mesDelAño !="Marzo" && mesDelAño !="Julio" && mesDelAño !="Diciembre")
+	{
+		alert("que comiences bien el año!!!.")
+	}
+
+	else
+	{
+		if(mesDelAño ="Marzo" && mesDelAño !="Julio" && mesDelAño !="Diciembre")
+	{
+		alert("a clases..");
+	}
+
+	    else 
+		{
+          if(mesDelAño = "Julio" )
+		  {
+			alert("se vienen las vacaciones!!!.")
+		  }
+		}
+	}
+
 
 
 
