58d14b621ffde6636bbd62f4d22822903a51bd3e
magikboy <massimo.bosco26@gmail.com>
2023-02-07 15:33:10 -0300

    x

--- 2-InstruccionIf/jsInstruccionIF-08.js
+++ 2-InstruccionIf/jsInstruccionIF-08.js
@@ -1,8 +1,19 @@
+//Bosco Mascaro Massimo Ariel Div X
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
 	
+	let edad
 
+	let estado
+
+	estado=document.getElementById("estadoCivil").value;
+	
+	edad=parseInt(document.getElementById("txtIdEdad").value);
+	
+    if(edad >= 18 && estado != "Casado" && estado != "Divorciado")
+	{
+		alert("usted es soltero y no es menor");
+	}
+  
 
 }//FIN DE LA FUNCIÓN
