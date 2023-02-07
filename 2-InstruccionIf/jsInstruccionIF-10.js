0489256a932c3848023c511626cac868f1e31029
magikboy <massimo.bosco26@gmail.com>
2023-02-07 16:17:32 -0300

    a

--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,6 +1,24 @@
+//Bosco Mascaro Massimo Ariel Div X
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");
+
+  let random=Math.round(Math.random(1)*10);
+  
+  if (random >=9 && random <= 10)
+   {
+	alert("Exelente")
+   }
+   else
+	{
+		if(random >= 4 && random <=8 )
+	{
+		alert("Aprobo");
+	}
+
+	    else 
+		{
+          alert("vamos,la proxima se puede");
+		}
+	}
 
 }//FIN DE LA FUNCIÓN
