89cbf04a8207d8222d3c830855e864a05897aaa3
magikboy <massimo.bosco26@gmail.com>
2023-02-11 16:29:48 -0300

    da

--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -3,16 +3,50 @@ A.	 mostrar la cantidad de alambre a comprar  si se ingresara el largo y el anch
 B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un terreno circular y se debe alambra con tres hilos de alambre.
 C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
 */
-
+//Bosco Mascaro Massimo Ariel Div X
 function Rectangulo () 
 {
+  let largo;
+  let ancho;
+  let alambre;
+
+  largo=parseInt(document.getElementById("txtIdLargo").value);
+  ancho=parseInt(document.getElementById("txtIdAncho").value);
+  
+  alambre=(largo+ancho)*3;
+
+  alert("Usted tiene que comprar " + alambre + " metros de alambre");
 
 }
 function Circulo () 
 {
-	
+	let radio;
+    let alambre;
+
+    radio=parseInt(document.getElementById("txtIdRadio").value);
+
+    alambre= (radio * 3);
+
+    alert ("Usted tiene que comprar " + alambre + " metros de alambre");
+
+
 }
 function Materiales () 
 {
-	
+  let largo;
+  let ancho;
+  let m2;
+  let cal;
+  let cemento;
+
+  largo=parseInt(document.getElementById("txtIdLargo").value);
+  ancho=parseInt(document.getElementById("txtIdAncho").value);
+  
+  m2=(largo * ancho);
+
+  cal= m2 / 3
+
+  cemento= m2 / 2
+
+  alert("los metros cuadrados del terrenos son " + m2 + " y nesesita " + cal + " bolsas de cal como de " + cemento + " bolsas de cemento" ).toFixed(0);
 }
