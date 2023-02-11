e2a96b8d026993844c41988eb1caa512cbc3acd3
magikboy <massimo.bosco26@gmail.com>
2023-02-11 16:03:02 -0300

    1231

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -1,17 +1,51 @@
-/*1.	Para el departamento de facturación:
-A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
-B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
-C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
-*/
 function Sumar () 
 {
-    
+    let precio1;
+    let precio2;
+    let precio3;
+    let suma;
+
+    precio1=parseInt(document.getElementById("txtIdPrecioUno").value);
+    precio2=parseInt(document.getElementById("txtIdPrecioUno").value);
+    precio3=parseInt(document.getElementById("txtIdPrecioUno").value);
+
+    suma=(precio1 + precio2 + precio3);
+
+	alert (suma)
 }
 function Promedio () 
 {
-    
+	let precio1;
+    let precio2;
+    let precio3;
+    let Promedio;
+
+    precio1=parseInt(document.getElementById("txtIdPrecioUno").value);
+    precio2=parseInt(document.getElementById("txtIdPrecioUno").value);
+    precio3=parseInt(document.getElementById("txtIdPrecioUno").value);
+
+    Promedio=(precio1 + precio2 + precio3) / 3;
+
+	alert (Promedio)
 }
 function PrecioFinal () 
 {
-    
+	let precio1;
+    let precio2;
+    let precio3;
+    let suma;
+    let PrecioFinal;
+    let iva;
+
+    precio1=parseInt(document.getElementById("txtIdPrecioUno").value);
+    precio2=parseInt(document.getElementById("txtIdPrecioUno").value);
+    precio3=parseInt(document.getElementById("txtIdPrecioUno").value);
+
+    suma=(precio1 + precio2 + precio3);
+
+    iva = (suma * 21 / 100)
+
+    PrecioFinal= (suma + iva )
+
+	alert (PrecioFinal)
 }
