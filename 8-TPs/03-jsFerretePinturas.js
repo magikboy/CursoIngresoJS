a26dbf839dc9376d380e6b7dcef2436149a322d8
magikboy <massimo.bosco26@gmail.com>
2023-02-11 16:40:37 -0300

    da

--- 8-TPs/03-jsFerretePinturas.js
+++ 8-TPs/03-jsFerretePinturas.js
@@ -2,12 +2,29 @@
 A.	Al ingresar una temperatura en Fahrenheit debemos mostrar la temperatura en Centígrados con un mensaje concatenado (ej.: " 32 Fahrenheit son 0 centígrados").
 B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en Fahrenheit (ej.: "0  centígrados son 32 Fahrenheit ").
 */
+//Bosco Mascaro Massimo Ariel Div X
 function FahrenheitCentigrados () 
 {
-	
+	let Fahrenheit
+    let cuenta
+
+    Fahrenheit=parseInt(document.getElementById("txtIdTemperatura").value);
+
+    cuenta= ((Fahrenheit - 32)* 5/9).toFixed(1);
+
+    alert("la tempreratura es " + cuenta);
+    
 }
 
 function CentigradosFahrenheit () 
 {
-	
+	let centígrados
+    let cuenta
+
+    centígrados=parseInt(document.getElementById("txtIdTemperatura").value);
+
+    cuenta= ((centígrados * 9/5) + 32).toFixed(1);
+
+    alert("la tempreratura es " + cuenta);
+    
 }
