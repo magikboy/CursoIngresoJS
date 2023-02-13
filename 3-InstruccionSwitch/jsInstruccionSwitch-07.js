f2114b2e14dbbbd23c52ffc550047bf31bb2491f
magikboy <massimo.bosco26@gmail.com>
2023-02-13 17:14:37 -0300

    d

--- 3-InstruccionSwitch/jsInstruccionSwitch-07.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-07.js
@@ -1,6 +1,28 @@
 function mostrar()
 {
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+	let destinoIngresado =txtIdDestino.value;
+
+	switch(destinoIngresado)
+	{
+		case("Bariloche"):
+		{
+			alert("Oeste");
+			break
+		}
+		case("Cataratas"):
+		{
+			alert("Norte");
+			break
+		}
+		case("Ushuaia"):
+		{
+			alert("Sur");
+			break
+		}
+		case("Mar del plata"):
+		{
+			alert("Este");
+		}
+	}
 
 }//FIN DE LA FUNCIÓN
