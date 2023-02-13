ea65c23d02b49049415aba7502ebb8ffad500389
magikboy <massimo.bosco26@gmail.com>
2023-02-13 17:09:23 -0300

    da

--- 3-InstruccionSwitch/jsInstruccionSwitch-06.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-06.js
@@ -15,7 +15,36 @@ function mostrar()
 		{
 			alert("es de mañana");
 			break
-		}	
+		}
+		case(12):
+		case(13):
+		case(14):
+		case(15):
+		case(16):
+		case(17):
+		case(18):
+		case(19):
+		{
+			alert("es de tarde");
+			break
+		}
+		case(20):
+		case(21):
+		case(22):
+		case(23):
+		case(24):
+		case(1):
+		case(2):
+		case(3):
+		case(4):
+		case(5):
+		case(6):
+			{
+				alert("es de noche");
+				break
+			}
+	    default:
+			alert("no es una hora valida")
 	}
 
 }//FIN DE LA FUNCIÓN
