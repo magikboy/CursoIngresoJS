ba059014daef1a5ad9d6303ea22f12431e912602
magikboy <massimo.bosco26@gmail.com>
2023-02-14 15:05:58 -0300

    da

--- 3-InstruccionSwitch/jsInstruccionSwitch-10.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-10.js
@@ -1,6 +1,51 @@
+//Bosco Mascaro Massimo Ariel Div X
 function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+	//declaro las variables
+	let destino
+	let estacion 
 
+	//tomo su id del html
+	estacion = document.getElementById("txtIdEstacion").value
+	destino = document.getElementById("txtIdDestino").value
+
+	switch(estacion)
+	{
+		case "Invierno":
+			if (destino == "Bariloche")
+			{
+				alert("se viaja");
+			}
+			else 
+			{
+				alert("no se viaja");
+			}
+			break;
+		case "Verano":
+			if (destino == "Mar del plata" || destino == "Cataratas")
+			{
+				alert("se viaja");
+			}
+			else 
+			{
+				alert("no se viaja");
+			}
+			break;
+		case "Primavera":
+			if (destino == "Bariloche")
+			{
+				alert("no se viaja");
+			}
+			else 
+			{
+				alert("se viaja");
+			}
+			break
+		case "Otoño":	
+			if (destino == "Cordoba" || destino == "Cataratas" || destino == "Mar del plata" || destino == "Bariloche")
+			{
+				alert ("se viaja");
+			}
+			break;
+	}
 }//FIN DE LA FUNCIÓN
