73b141cdb87595fab2ab97792388fc8d17be8735
magikboy <massimo.bosco26@gmail.com>
2023-02-16 12:10:50 -0300

    31

--- 5-InstruccionWhile/jsIteracionesWhile-01.js
+++ 5-InstruccionWhile/jsIteracionesWhile-01.js
@@ -1,7 +1,12 @@
-/*
-al presionar el botón mostrar 10 repeticiones 
+/*al presionar el botón mostrar 10 repeticiones 
 con números ASCENDENTE, desde el 1 al 10.*/
+//Bosco Mascaro Massimo Ariel Div X
 function mostrar()
 {
-	alert('iteración while');
+	let i = 1
+	while(i <= 10)
+	{
+		alert(i)
+		i ++ ;
+	}
 }//FIN DE LA FUNCIÓN
