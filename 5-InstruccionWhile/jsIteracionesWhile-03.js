922b3961df2475cac0f366e2339a7176cfd46c56
magikboy <massimo.bosco26@gmail.com>
2023-02-16 14:20:46 -0300

    da

--- 5-InstruccionWhile/jsIteracionesWhile-03.js
+++ 5-InstruccionWhile/jsIteracionesWhile-03.js
@@ -1,8 +1,15 @@
 /*
 al presionar el botón pedir la CLAVE (ayuda: es utn750)*/
+//Bosco Mascaro Massimo Ariel Div X
 function mostrar()
 {
-	var claveIngresada;
-	claveIngresada = prompt("ingrese el número clave.");
+	let claveIngresada;
+	claveIngresada = prompt("ingrese la clave.");
+
+	while(claveIngresada == "utn750")
+	{
+		alert ("su clave es correcta");
+		break
+	}
 	
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteracionesWhile-04.js
+++ 5-InstruccionWhile/jsIteracionesWhile-04.js
@@ -4,6 +4,12 @@ pedir un número entre 0 y 9 inclusive.*/
 function mostrar()
 {
 	var numeroIngresado;
-	numeroIngresado = prompt("ingrese un número entre 0 y 10.");
+	numeroIngresado = prompt("ingrese un número entre 0 y 9.");
+	let i = 0
 	
+	while(i >= 0 && i <= 9)
+	{
+		alert(numeroIngresado);
+		br
+	}
 }//FIN DE LA FUNCIÓN
