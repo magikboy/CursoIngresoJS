884621d318ad84efd18626c4a69e8f1a3ebd4ccb
magikboy <massimo.bosco26@gmail.com>
2023-02-22 14:24:06 -0300

    we

--- 5-InstruccionWhile/jsIteracionesWhile-10.js
+++ 5-InstruccionWhile/jsIteracionesWhile-10.js
@@ -10,20 +10,77 @@ hasta que el usuario quiera, mostrar:
 7-Promedio de positivos.
 8-Promedios de negativos.
 9-Diferencia entre positivos y negativos, (positvos-negativos). */
+//Bosco Mascaro Massimo Ariel Div X
 function mostrar()
 {
-	//declarar contadores y variables 
-	var respuesta;
-	var numeroIngresado;
-	var sumaNegativos=0;
+	let numeroIngresado;
+	let seguir;
+	let sumaNegativos;
+	let sumaPositivos;
+	let contadorPositivos;
+	let contadorNegativos;
+	let contadorCeros;
+	let contadorPares;
+	let promedioPositivos;
+	let promedioNegativos;
+	let diferenciaPositivosNegativos;
 
-	respuesta="si";
 
-	while(respuesta=="si")
+	contadorPositivos=0;	
+	contadorNegativos=0;
+	contadorCeros=0;
+	contadorPares=0;	
+	sumaNegativos=0;
+	sumaPositivos=0;
+	promedioPositivos=0;
+	promedioNegativos=0;
+	diferenciaPositivosNegativos=0;
+	seguir='si';
+
+	while(seguir !='no')
 	{
-		
-		respuesta=prompt("desea continuar?");
-	}//fin del while
+		numeroIngresado=parseInt(prompt("Ingrese un número"));
+		if(numeroIngresado>0)
+		{
+			sumaPositivos=sumaPositivos+numeroIngresado;
+			contadorPositivos++;
+		}
+		else
+		{
+			if(numeroIngresado<0)
+			{
+				sumaNegativos=sumaNegativos+numeroIngresado;
+				contadorNegativos++;
+			}
+			else
+			{
+				contadorCeros++;
+			}
+		}
+		if(numeroIngresado%2==0)
+		{
+			contadorPares++;
+		}
+		seguir=prompt("¿Desea continuar?");
+	}
 
-	document.write("la suma de negativos es :"+sumaNegativos);
+	promedioPositivos=sumaPositivos/contadorPositivos;
+
+	promedioNegativos=sumaNegativos/contadorNegativos;
+	diferenciaPositivosNegativos=promedioPositivos-promedioNegativos;
+
+	document.write("1-Suma de los negativos: "+sumaNegativos+"<br>");
+	document.write("2-Suma de los positivos: "+sumaPositivos+"<br>");
+	document.write("3-Cantidad de positivos: "+contadorPositivos+"<br>");
+	document.write("4-Cantidad de negativos: "+contadorNegativos+"<br>");
+	document.write("5-Cantidad de ceros: "+contadorCeros+"<br>");
+	document.write("6-Cantidad de números pares: "+contadorPares+"<br>");
+	document.write("7-Promedio de positivos: "+promedioPositivos+"<br>");
+	document.write("8-Promedios de negativos: "+promedioNegativos+"<br>");
+	document.write("9-Diferencia entre positivos y negativos: "+diferenciaPositivosNegativos+"<br>");
+	document.write("<br>");
+	document.write("<br>");
+	document.write("<br>");
+	document.write("<br>");
+	document.write("<br>");
 }//FIN DE LA FUNCIÓN
