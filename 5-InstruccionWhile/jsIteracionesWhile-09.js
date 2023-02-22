125086fabaf01c7c379c4c637344bea56f1d9dbb
magikboy <massimo.bosco26@gmail.com>
2023-02-22 12:55:05 -0300

    fa

--- 5-InstruccionWhile/jsIteracionesWhile-09.js
+++ 5-InstruccionWhile/jsIteracionesWhile-09.js
@@ -1,21 +1,41 @@
 /*
 Al presionar el botón pedir  números  hasta que el usuario quiera,
 mostrar el número máximo y el número mínimo ingresado.*/
+//Bosco Mascaro Massimo Ariel Div X
 function mostrar()
 {	// declarar variables
-	var banderaDelPrimero;
-	var numeroIngresado;
-	var numeroMaximo;
-	var numeroMinimo;
-	var respuesta;
-	//iniciar variables
-	banderaDelPrimero="es el primero";
-	respuesta='si';
-	while(respuesta=="si")
+	let numeroIngresado;
+	let contador;
+	let seguir;
+	let numeroMaximo;
+	let numeroMinimo;
+	contador=0;
+	seguir='si';
+	numeroMaximo=0;
+	numeroMinimo=0;
+    // recorrer el array
+	while(seguir !='no')
 	{
-		
-		respuesta=prompt("desea continuar?");
+		numeroIngresado=parseInt(prompt("Ingrese un número"));
+		if(contador==0)
+		{
+			numeroMaximo=numeroIngresado;
+			numeroMinimo=numeroIngresado;
+		}
+		else
+		{
+			if(numeroIngresado>numeroMaximo)
+			{
+				numeroMaximo=numeroIngresado;
+			}
+			if(numeroIngresado<numeroMinimo)
+			{
+				numeroMinimo=numeroIngresado;
+			}
+		}
+		contador++;
+		seguir=prompt("¿Desea continuar?");
 	}
-	txtIdMaximo.value=numeroMaximo;
-	txtIdMinimmo.value=numeroMinimo;
+	document.getElementById("txtIdMaximo").value=numeroMaximo;
+	document.getElementById("txtIdMinimo").value=numeroMinimo;
 }//FIN DE LA FUNCIÓN
