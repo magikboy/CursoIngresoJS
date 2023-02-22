09b83d0fab387d8d68f9807a5dfd40b852368ce1
magikboy <massimo.bosco26@gmail.com>
2023-02-22 12:05:45 -0300

    fa

--- 5-InstruccionWhile/jsIteracionesWhile-08.js
+++ 5-InstruccionWhile/jsIteracionesWhile-08.js
@@ -1,19 +1,38 @@
 /*
 Al presionar el botón pedir  números  hasta que el usuario quiera,
 sumar los que son positivos y multiplicar los negativos.*/
+//Bosco Mascaro Massimo Ariel Div X
 function mostrar()
 {
-	var contador;
-	var respuesta;
-	var sumaPositivos;
-	var multiplicacionNegativos;
+	let numeroIngresado;
+	let contador;
+	let seguir;
+	let sumaPositivos;
+	let multiplicacionNegativos;
 	contador=0;
 	sumaPositivos=0;
 	multiplicacionNegativos=1;
-	respuesta='si';
+	seguir='si';
 
 
-	txtIdSuma.value=sumaPositivos;
-	txtIdProducto.value=multiplicacionNegativos;
+
+	while(seguir !='no')
+    {
+        numeroIngresado=parseInt(prompt("Ingrese un número"));
+        if(numeroIngresado>0)
+        {
+            sumaPositivos=sumaPositivos+numeroIngresado;
+            contador++;
+        }
+        else
+        {
+            multiplicacionNegativos=multiplicacionNegativos*(-1);
+            contador++;
+        }
+        seguir=prompt("¿Desea continuar?");
+    }
+
+	document.getElementById("txtIdSuma").value=sumaPositivos;
+	document.getElementById("txtIdProducto").value=multiplicacionNegativos*contador;
 
 }//FIN DE LA FUNCIÓN
